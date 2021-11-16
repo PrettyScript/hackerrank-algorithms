e5b76b30debf2e0f583d80e2666479e6e40e4201
PrettyScript <JessicaDelcy@gmail.com>
2021-11-16 14:59:32 -0600

    completed array challenge

---
+++ arrays/staircase.js
@@ -0,0 +1,47 @@
+/** Write a program that prints a staircase of size .
+
+Function Description
+
+Complete the staircase function in the editor below.
+
+staircase has the following parameter(s):
+
+int n: an integer
+Print
+
+Print a staircase as described above.
+
+Input Format
+
+A single integer, , denoting the size of the staircase.
+
+Constraints
+
+ .
+
+Output Format
+
+Print a staircase of size  using # symbols and spaces.
+
+Note: The last line must have  spaces in it.
+
+Sample Input
+
+6 
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of .  */
+
+function staircase(n) {
+    for (let i = 1; i < n + 1; i++) {
+        console.log(" ".repeat(n - i) + "#".repeat(i));
+    }
+}
